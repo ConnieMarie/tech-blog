7444c7a8950aa8d6944e2945489d15f43cd04ef2
ConnieMarie <constance.marie.strahan@gmail.com>
2022-10-19 15:57:50 -0500

    fixing heroku session error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SESSION_SECRET,
+  secret: 'SESSION_SECRET',
   cookie: {  
     maxAge: 60000,
     overwrite: false
